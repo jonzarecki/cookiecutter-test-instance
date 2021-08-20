e3a8c0a778d2e59138c6fd9db60ba11dae63a5c4
Jonathan Zarecki <yonilx@gmail.com>
2021-08-20 22:31:51 +0300

    ci: update template
    update template.

---
+++ notebooks/nb_python/example_note.py
@@ -0,0 +1,24 @@
+# %% [markdown]
+# hello world
+
+# %%
+a = 1
+print(a)
+print(a)
+print(a)
+
+# %%
+
+a = 5123
+b = 2
+print(a)
+print(b)
+
+# %%
+a = 322
+print(a)
+
+# %%
+print("hello world")
+
+# %%
