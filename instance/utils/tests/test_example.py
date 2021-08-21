d52a36e0758699ae1eec48f9fddc86d41ad96afd
Jonathan Zarecki <yonilx@gmail.com>
2021-08-21 18:14:35 +0300

    test: check coverage behaviour with utils
    change location and pyproj config

---
+++ instance/utils/tests/test_example.py
@@ -0,0 +1,14 @@
+# pylint: disable=redefined-outer-name
+import pytest
+
+from instance.utils.example_util import _util_function
+
+
+@pytest.fixture
+def setup1() -> int:
+    return _util_function()
+
+
+def test_something(setup1: int) -> None:
+    print(setup1)
+    assert setup1 == 3, "always right"
